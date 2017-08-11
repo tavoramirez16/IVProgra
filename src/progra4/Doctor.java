7b41216fd365e331198a6710cf1dba0d5485fc09
yor-araya <yor.araya10@gmail.com>
2017-08-10 22:46:15 -0600

    Mejora código.

--- src/progra4/Doctor.java
+++ src/progra4/Doctor.java
@@ -65,6 +65,37 @@ public class Doctor {
     }//fin metodo setCorreo   
    
     /*metodo para agregar un doctor*/
+     public boolean agregar(String datos){
+        boolean agregado = false; 
+                if(conect.agregar(tabla,datos)){
+            agregado = true;
+        }//fin del if
+        return agregado;
+    }//fin metodo agregar
+    /*metodo para editar algun dato de un doctor*/
+    public boolean editar(String datos, String condicion){
+        boolean editado = false;
+        if(conect.editar(tabla,datos,condicion)){
+            editado = true;
+        }//fin del if
+        return editado;
+    }//fin metodo editar   
+    /*metodo para eliminar un doctor*/
+    public boolean eliminar(String condicion){
+        boolean eliminado = false;
+        if(conect.eliminar(tabla,condicion)){
+            eliminado = true;
+        }//fin del if
+        return eliminado;
+    }//fin metodo eliminar   
+    /*metodo para imprimir todos los doctores*/
+    public String [][] imprimir(String datos, String condicion, String order){
+        String [][] resultado = conect.imprimir(tabla,datos,condicion, order);
+        return resultado;
+    }//fin metodo imprimir
+   
+  /**
+    /*metodo para agregar un doctor*/
      public boolean agregar(){
         boolean agregado = false;
         idDoctor= 0;
@@ -115,4 +146,5 @@ public class Doctor {
             }//fin for interno
         }//fin for
     }//fin metodo imprimir
+ **/
 }//fin clase Doctor
